30882f9656fa232fccf9cd3a62352bed33ed3099
Maciej Mirowski <maciej.mir@wp.pl>
2025-12-18 11:17:23 +0100

    sus arches deleted

--- arches_for_excavation/arches_slocal/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArchesSlocalConfig(AppConfig):
-    name = "arches_slocal"
-    is_arches_application = True
--- arches_for_excavation/arches_slocal/hosts.py
+++
@@ -1,7 +0,0 @@
-import re
-from django_hosts import patterns, host
-
-host_patterns = patterns(
-    "",
-    host(re.sub(r"_", r"-", r"arches_slocal"), "arches_slocal.urls", name="arches_slocal"),
-)
