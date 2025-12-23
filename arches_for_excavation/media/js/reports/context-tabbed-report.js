00790731ff41dc53e7073f295c892b8b02180c1c
fszafran <filo1234@interia.pl>
2025-12-23 11:44:44 +0100

    Parent ann

--- arches_for_excavation/media/js/reports/context-tabbed-report.js
+++ arches_for_excavation/media/js/reports/context-tabbed-report.js
@@ -9,11 +9,13 @@ import resourceService from '../services/resource-service';
 export default ko.components.register('context-tabbed-report', {
     viewModel: function(params) {
         const self = this;
+
+        const ANNOTATION_RESOURCE_GRAPHID = '2880934b-0015-4c5a-8ec1-1ab9bca329fd';
        
         // 3D Models state
         self.models3D = ko.observableArray([]);
         self.allowAnnotationsEdits = ko.observable(false);
-        self.allowObjectPicking = ko.observable(false);
+        self.allowObjectPicking = ko.observable(true);
         self.existingAnnotations = ko.observableArray([]);
        
         // IIIF state
@@ -60,17 +62,59 @@ export default ko.components.register('context-tabbed-report', {
                     }
                 }));
        
-                actualModels.forEach(modelResource => {
-                    if (modelResource) {
-                        const resourceId = modelResource.link.split('/').pop();
-                        console.log("[CONTEXT REPORT] Processing 3D model resource id:", resourceId);
-                       
-                        resourceService.getOne(resourceId).then(data => {
-                            data.resourceId = resourceId;
-                            self.models3D.push(data);
-                            console.log("[CONTEXT REPORT] Added 3D model to models3D:", data);
+                const modelPromises = actualModels.map(modelResource => {
+                    if (!modelResource) return Promise.resolve(null);
+                    
+                    const resourceId = modelResource.link.split('/').pop();
+                    console.log("[CONTEXT REPORT] Processing 3D model resource id:", resourceId);
+
+                    return Promise.all([
+                        resourceService.getOne(resourceId),
+                        resourceService.getAllRelatedTo(resourceId)
+                    ]).then(([modelData, relatedData]) => {
+                        console.log("[CONTEXT REPORT] Model data:", modelData);
+                        console.log("[CONTEXT REPORT] Related resources:", relatedData);
+                        
+                        const relatedResourcesArray = relatedData?.related_resources?.related_resources || [];
+                        console.log("[CONTEXT REPORT] Related resources array:", relatedResourcesArray);
+                        
+                        const annotationResources = relatedResourcesArray
+                            .filter(related => related.graph_id === ANNOTATION_RESOURCE_GRAPHID);
+                        
+                        console.log("[CONTEXT REPORT] Annotation resources for model:", annotationResources);
+                        
+                        const annotationPromises = annotationResources.map(anno => 
+                            resourceService.getOne(anno.resourceinstanceid)
+                        );
+                        
+                        return Promise.all(annotationPromises).then(annotations => {
+                            const annotationsWithModelId = annotations.map(anno => ({
+                                ...anno,
+                                modelResourceId: resourceId
+                            }));
+                            
+                            console.log("[CONTEXT REPORT] Fetched annotations with model ID:", annotationsWithModelId);
+                            
+                            self.models3D.push(modelData);
+                            
+                            annotationsWithModelId.forEach(anno => {
+                                self.existingAnnotations.push(anno);
+                            });
+                            
+                            console.log("[CONTEXT REPORT] Added model and annotations");
+                            
+                            return {
+                                model: modelData,
+                                annotations: annotationsWithModelId
+                            };
                         });
-                    }
+                    });
+                });
+                
+                Promise.all(modelPromises).then(results => {
+                    console.log("[CONTEXT REPORT] All models and annotations loaded:", results);
+                }).catch(error => {
+                    console.error("[CONTEXT REPORT] Error loading models/annotations:", error);
                 });
             }
         }
