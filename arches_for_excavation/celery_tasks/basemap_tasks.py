5abb4f387796ab203d01d6c3eaa341457956b47a
fszafran <filo1234@interia.pl>
2026-02-25 10:48:18 +0100

    all band count basemaps handling

--- arches_for_excavation/celery_tasks/basemap_tasks.py
+++ arches_for_excavation/celery_tasks/basemap_tasks.py
@@ -41,7 +41,8 @@ def register_basemap_in_db(basemap_metadata):
             'id': basemap_metadata['id'],
             'type': 'raster',
             'source': basemap_metadata['id'],
-            'basemap_dir': basemap_metadata['sanitized_name']
+            'basemap_dir': basemap_metadata['sanitized_name'],
+            'band_count': basemap_metadata['band_count']
         }],
         isoverlay=basemap_metadata['isoverlay'],
         activated=basemap_metadata['activated'],
