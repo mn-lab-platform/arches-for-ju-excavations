c9fc46a4d6ac9ff86111aa7c37ba67ccdd8c05d0
fszafran <filo1234@interia.pl>
2026-03-04 13:40:32 +0100

    change tilesize to 256 globally

--- arches_for_excavation/celery_tasks/basemap_tasks.py
+++ arches_for_excavation/celery_tasks/basemap_tasks.py
@@ -28,7 +28,7 @@ def register_basemap_in_db(basemap_metadata):
         source={
             'type': 'raster',
             'tiles': [f'/api/titiler/tiles/{basemap_metadata["id"]}/{{z}}/{{x}}/{{y}}'],
-            'tileSize': 512,
+            'tileSize': 256,
             'bounds': basemap_metadata['bounds']
         }
     )
