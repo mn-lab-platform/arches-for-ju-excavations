7c05d0efc7fa3e2d6c1ca14a5dc0acbbfa65c257
fszafran <filo1234@interia.pl>
2026-04-21 12:16:53 +0200

    restore previous back to default tool behaviour in cesium viewer

--- arches_for_excavation/media/js/cesium_viewer/cesium/tools/BackToDefaultTool.js
+++ arches_for_excavation/media/js/cesium_viewer/cesium/tools/BackToDefaultTool.js
@@ -8,14 +8,20 @@ export class BackToDefaultTool extends Tool {
 
     activate() {
         this.active = true;
+        
+        const onDone = () => {
+            this._triggerCallback(TOOL_CALLBACKS.ON_TOOL_SELF_DEACTIVATE);
+        };
+
         this.widget.camera.flyTo({
             destination: this.cameraDestination,
             orientation: {
                 heading: 0.0,
                 roll: 0.0
-            }
+            },
+            complete: onDone,
+            cancel: onDone
         });
-        this._triggerCallback(TOOL_CALLBACKS.ON_TOOL_SELF_DEACTIVATE);
     }
 
     deactivate() {
