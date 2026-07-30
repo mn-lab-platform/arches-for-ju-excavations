bb770a5bf13a79bd8a35928aced21241e8c2f54a
fszafran <filo1234@interia.pl>
2026-07-30 16:06:47 +0200

    remove redundant arches urls support (handled in wrapper)

--- arches_for_excavation/urls.py
+++ arches_for_excavation/urls.py
@@ -84,19 +84,3 @@ urlpatterns = [
     path("api/resource-mapping/suggest", ResourceMappingSuggestView.as_view(), name="resource_mapping_suggest"),
     path("api/resource-mapping/migrate", ResourceMappingMigrateView.as_view(), name="resource_mapping_migrate"),
 ]
-
-    
-# Ensure Arches core urls are superseded by project-level urls
-urlpatterns.append(path("", include("arches.urls")))
-
-
-# Adds URL pattern to serve media files during development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Only handle i18n routing in active project. This will still handle the routes provided by Arches core and Arches applications,
-# but handling i18n routes in multiple places causes application errors.
-if settings.ROOT_URLCONF == __name__:
-    if settings.SHOW_LANGUAGE_SWITCH is True:
-        urlpatterns = i18n_patterns(*urlpatterns)
-
-    urlpatterns.append(path("i18n/", include("django.conf.urls.i18n")))
